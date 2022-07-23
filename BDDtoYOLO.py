b59f4851ec86f3c463b3ca4a99c7ca2668867a09
Ravi Patibanda <pvraviteja@hotmail.com>
2022-07-23 04:26:35 +0200

    CHG add BDD to YOLO conversion function CHG add generic format types

---
+++ BDDtoYOLO.py
@@ -0,0 +1,27 @@
+import os
+
+from format import BDD,YOLO
+
+def convertBDDtoYOLO(dict:config) -> bool:
+    if config["datasets"] != "BDD":
+        print("Unknown Dataset")
+		return False
+		
+	bdd = BDD()
+	yolo = YOLO(os.path.abspath(config["cls_list"]))
+
+	success, data = bdd.parse(config["label"], config["img_path"])
+	if not success:
+		print("BDD Parsing Result : {}, msg : {}".format(success, data))
+		return False
+
+	success, data = yolo.generate(data)
+	if not success:
+		print("YOLO Generating Result : {}, msg : {}".format(success, data))
+		return False
+		
+	success, data = yolo.save(data, config["output_path"], config["img_path"],
+							config["img_type"], config["manifest_path"])
+	if success:
+		print("Saving Result : {}, msg : {}".format(success, data))
+	return success
