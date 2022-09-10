c2ff8848e0c8f502d93ccd06fc0c708fb0ba1ac0
Ravi Patibanda <pvraviteja@hotmail.com>
2022-09-10 06:45:38 +0200

    FIX avoid namespace collision with other models

--- models/yolo.py
+++ models/yolo.py
@@ -6,10 +6,10 @@ from pathlib import Path
 import torch
 import torch.nn as nn
 
-from models.common import *
-from models.experimental import MixConv2d, CrossConv, C3
-from utils.general import check_anchor_order, make_divisible, check_file
-from utils.torch_utils import (
+from ScaledYOLOv4.models.common import *
+from ScaledYOLOv4.models.experimental import MixConv2d, CrossConv, C3
+from ScaledYOLOv4.utils.general import check_anchor_order, make_divisible, check_file
+from ScaledYOLOv4.utils.torch_utils import (
     time_synchronized, fuse_conv_and_bn, model_info, scale_img, initialize_weights, select_device)
 
 
